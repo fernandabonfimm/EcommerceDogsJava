21431276f982fb95f561fbc3101770a34499f623
pedro <pedroubine@hotmail.com>
2023-04-25 16:22:43 -0300

    faltou exames.java

---
+++ classes/src/SaudeDoAnimal/Exams/Exames.java
@@ -0,0 +1,10 @@
+package src.SaudeDoAnimal.Exams;
+
+import java.util.Map;
+
+public interface Exames {
+    Map<String, Boolean> exame = null;
+
+//    public boolean verificarExames();
+    public void  MostrarDados();
+}
